fac37a2885633067bd108c22e7efcb76e05d856a
Balintus00 <traxlerbalint@gmail.com>
2020-09-29 10:54:45 +0200

    A hírek, mint listaelemek struktúrájának megvalósítása

---
+++ TemalaborSimpleNewsReader/app/src/main/java/hu/bme/aut/android/temalaborsimplenewsreader/features/newsfeed/NewsFeedActivity.kt
@@ -0,0 +1,12 @@
+package hu.bme.aut.android.temalaborsimplenewsreader.features.newsfeed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.bme.aut.android.temalaborsimplenewsreader.R
+
+class NewsFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_feed)
+    }
+}

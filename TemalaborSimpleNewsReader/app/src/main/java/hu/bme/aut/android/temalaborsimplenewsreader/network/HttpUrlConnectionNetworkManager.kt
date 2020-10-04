55e6b656ee072009f96f705a22e6371cd787ddeb
Balintus00 <traxlerbalint@gmail.com>
2020-10-04 18:25:23 +0200

    A network package tartalmának dokumentálása (KDoc)

--- TemalaborSimpleNewsReader/app/src/main/java/hu/bme/aut/android/temalaborsimplenewsreader/network/HttpUrlConnectionNetworkManager.kt
+++ TemalaborSimpleNewsReader/app/src/main/java/hu/bme/aut/android/temalaborsimplenewsreader/network/HttpUrlConnectionNetworkManager.kt
@@ -7,12 +7,36 @@ import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.SocketTimeoutException
 
+/**
+ * This object implements the INetworkManager interface with using HttpUrlConnection library.
+ */
 object HttpUrlConnectionNetworkManager : INetworkManager {
 
+    /**
+     * This member holds in milliseconds the connection timeout threshold. If it expires, the
+     * connection will throw a SocketTimeoutException.
+     */
     private const val connectTimeoutValue = 3000
+
+    /**
+     * When reading from the returned input stream, if the read timeout, which is held by this
+     * member, expires before data is available for read, the HttpUrlConnection should throw a
+     * SocketTimeoutException.
+     */
     private const val readTimeoutValue = 3000
 
+    /**
+     * With this function, http get requests can be sent to the specified address. The functionality
+     * is implemented by using HttpUrlConnection library.
+     * @param urlAddress the get request's address.
+     * @return the response's body.
+     * @throws SocketTimeoutException if the HttpUrlConnection instance's connect timeout or
+     * read timeout expires.
+     * @throws IOException if some problem happens with reading the InputStream, which contains
+     * the response's body.
+     */
     //Using Dispatchers.IO should solve this problem
     @Suppress("BlockingMethodInNonBlockingContext")
     override suspend fun getHttpAnswer(urlAddress: String) = withContext(Dispatchers.IO) {
--- TemalaborSimpleNewsReader/app/src/main/java/hu/bme/aut/android/temalaborsimplenewsreader/network/INetworkManager.kt
+++ TemalaborSimpleNewsReader/app/src/main/java/hu/bme/aut/android/temalaborsimplenewsreader/network/INetworkManager.kt
@@ -1,6 +1,17 @@
 package hu.bme.aut.android.temalaborsimplenewsreader.network
 
+/**
+ * INetworkManager's responsibility to provide an interface to use network communication.
+ */
 interface INetworkManager {
 
+    /**
+     * This function's implementations sends a http get request to the specified address,
+     * and returns with the response's body. This function is a suspend function, because
+     * the implementations should use network communication to send the get request, and
+     * get the response.
+     * @param urlAddress the address of the http get message.
+     * @return the body of the http response.
+     */
     suspend fun getHttpAnswer(urlAddress: String): String
 }

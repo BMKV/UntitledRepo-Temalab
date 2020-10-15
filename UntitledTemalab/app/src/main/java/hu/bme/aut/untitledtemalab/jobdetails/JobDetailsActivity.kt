e644361bae33d9b4f27c9765406110de463f6989
BMKV <47791713+BMKV@users.noreply.github.com>
2020-10-15 19:46:30 +0200

    Added Activities/Fragments
    MainMenu, ProfileScreen, JobDetails Activity-k Fragmentek hozzáadva

---
+++ UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/jobdetails/JobDetailsActivity.kt
@@ -0,0 +1,12 @@
+package hu.bme.aut.untitledtemalab.jobdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.bme.aut.untitledtemalab.R
+
+class JobDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_details)
+    }
+}
---
+++ UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/jobdetails/PostJobActivity.kt
@@ -0,0 +1,12 @@
+package hu.bme.aut.untitledtemalab.jobdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.bme.aut.untitledtemalab.R
+
+class PostJobActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_job)
+    }
+}

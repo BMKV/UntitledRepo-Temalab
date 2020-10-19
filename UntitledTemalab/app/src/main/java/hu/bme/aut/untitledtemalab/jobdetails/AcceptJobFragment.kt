c4563516b946111b4644fe3686851cdec6040325
BMKV <47791713+BMKV@users.noreply.github.com>
2020-10-19 14:36:02 +0200

    GUI fejlesztés 3
    JobDetails fragmentek override fv-ei, PostJob Activity GUI

--- UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/jobdetails/AcceptJobFragment.kt
+++ UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/jobdetails/AcceptJobFragment.kt
@@ -1,4 +1,20 @@
 package hu.bme.aut.untitledtemalab.jobdetails
 
-class AcceptJobFragment {
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import hu.bme.aut.untitledtemalab.R
+
+class AcceptJobFragment :Fragment() {
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_accept_job, container, false)
+    }
 }
--- UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/jobdetails/ChangeJobStatusFragment.kt
+++ UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/jobdetails/ChangeJobStatusFragment.kt
@@ -1,4 +1,19 @@
 package hu.bme.aut.untitledtemalab.jobdetails
 
-class ChangeJobStatusFragment {
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import hu.bme.aut.untitledtemalab.R
+
+class ChangeJobStatusFragment :Fragment() {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_change_job_status, container, false)
+    }
 }

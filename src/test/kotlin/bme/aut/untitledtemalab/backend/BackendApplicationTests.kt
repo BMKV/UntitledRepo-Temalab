365d1b5459721ab374d182b3485f4325403a02c3
xShipi <sipisipos@gmail.com>
2020-10-25 21:14:38 +0100

    Beginning...
    Backend kezdetek: kindulas, heroku szuksegseges dependency-k

---
+++ src/test/kotlin/bme/aut/untitledtemalab/backend/BackendApplicationTests.kt
@@ -0,0 +1,15 @@
+package bme.aut.untitledtemalab.backend
+
+// TEST: TODO
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+//@SpringBootTest
+//class BackendApplicationTests {
+
+//    @Test
+//    fun contextLoads() {
+//   }
+
+//}

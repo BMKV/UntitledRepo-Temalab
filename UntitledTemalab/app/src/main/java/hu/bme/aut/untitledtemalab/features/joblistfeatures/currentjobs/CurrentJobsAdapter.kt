ed2aa2c9a101279b6d2a6ba10613a4c9336693cb
Balintus00 <traxlerbalint@gmail.com>
2020-11-08 20:47:54 +0100

    Removed specific joblist feature RecyclerView Adapter classes

--- UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/features/joblistfeatures/currentjobs/CurrentJobsAdapter.kt
+++
@@ -1,9 +0,0 @@
-package hu.bme.aut.untitledtemalab.features.joblistfeatures.currentjobs
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-class CurrentJobsAdapter {
-
-
-}

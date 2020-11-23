dcc477909b019def42319adc0f999276db92683c
Balintus00 <traxlerbalint@gmail.com>
2020-11-23 10:16:46 +0100

    ProfileScreenRepository implemented

--- UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/features/profile/ProfileScreenRepository.kt
+++ UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/features/profile/ProfileScreenRepository.kt
@@ -1,11 +1,17 @@
 package hu.bme.aut.untitledtemalab.features.profile
 
+import hu.bme.aut.untitledtemalab.network.NetworkManager
 import hu.bme.aut.untitledtemalab.network.response.UserDataResponse
 
 class ProfileScreenRepository(private val userId: Long) {
 
     suspend fun getUserData(): UserDataResponse{
-        TODO()
+        return try{
+            UserDataResponse(NetworkManager.getUserProfileById(userId), null)
+        }
+        catch (exception: Exception){
+            UserDataResponse(null, exception)
+        }
     }
 
 }

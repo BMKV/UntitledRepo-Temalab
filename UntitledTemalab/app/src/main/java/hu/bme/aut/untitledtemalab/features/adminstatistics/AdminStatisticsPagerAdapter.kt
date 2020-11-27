ac1ab3359e7d5f01be09b1a1ce3f82dcad7c957f
Balintus00 <traxlerbalint@gmail.com>
2020-11-27 16:23:10 +0100

    AdminStatistics container

---
+++ UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/features/adminstatistics/AdminStatisticsPagerAdapter.kt
@@ -0,0 +1,27 @@
+package hu.bme.aut.untitledtemalab.features.adminstatistics
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import java.lang.IllegalStateException
+
+class AdminStatisticsPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, val userId: Long) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    companion object{
+        private const val ADMIN_STATISTICS_PAGE_COUNT = 2
+    }
+
+    override fun getItemCount(): Int {
+        return ADMIN_STATISTICS_PAGE_COUNT
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0 -> AdminsUserRoleStatisticFragment.newInstance(userId)
+            1 -> AdminsJobStatusStatisticFragment.newInstance(userId)
+            else -> throw IllegalStateException("Such position doesn't exist: $position!")
+        }
+    }
+}
---
+++ UntitledTemalab/app/src/main/java/hu/bme/aut/untitledtemalab/features/adminstatistics/AdminsJobStatusStatisticFragment.kt
@@ -0,0 +1,47 @@
+package hu.bme.aut.untitledtemalab.features.adminstatistics
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hu.bme.aut.untitledtemalab.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AdminsJobStatusStatisticFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AdminsJobStatusStatisticFragment : Fragment() {
+
+    companion object {
+
+        private const val USER_ID_KEY = "USER_ID_KEY"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         * @param userId the owner user's ID.
+         * @return A new instance of fragment AdminsJobStatusStatisticFragment.
+         */
+        @JvmStatic
+        fun newInstance(userId: Long) =
+            AdminsJobStatusStatisticFragment().apply {
+                arguments = Bundle().apply {
+                    putLong(USER_ID_KEY, userId)
+                }
+            }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admins_job_status_statistic, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
